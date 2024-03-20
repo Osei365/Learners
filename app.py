0f48743ce9a8ad10ae74809fa61898982acfdfeb
osei <williamsabodunrin@gmail.com>
2024-03-20 16:51:08 +0100

    signup handling

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask
+import bcrypt
+from flask import Flask, request, abort, jsonify
 from api.v1.views import app_views
 from models import db
 from flask_login import LoginManager
@@ -38,9 +39,32 @@ def home():
     print('something')
     return 'hello world'
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    pass
+@app.route('/signup', methods=['POST'])
+def signup():
+    json_doc = request.get_json()
+    if not json_doc:
+        abort(404, description='Not a JSON')
+
+    if 'email' not in json_doc:
+        abort(404, description='email not present')
+
+    if 'password' not in json_doc:
+        abort(404, description='password not present')
+
+    json_doc['password'] = hash_password(json_doc['password'])
+    teacher = Teacher(**json_doc)
+    db.session.add(teacher)
+    db.session.commit()
+    result = jsonify({"email": "{}".format(json_doc['email']),
+                          "message": "user created"})
+
+
+def hash_password(password):
+    byte = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(byte, salt)
+    return hashed_password
+    
 
 
 if __name__ == "__main__":
--- models/teacher.py
+++ models/teacher.py
@@ -1,16 +1,18 @@
 from models import db
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey, Integer
 from flask_login import UserMixin
 
 
 class Teacher(db.Model, UserMixin):
 
     id = Column(String(120), primary_key=True)
-    name = Column(String(120), nullable=True)
+    first_name = Column(String(120), nullable=True)
+    last_name = Column(String(120), nullable=True)
+    phone_number = Column(Integer, nullable=True)
     email = Column(String(120), nullable=False)
     password = Column(String(120), nullable=False)
-    school = Column(String(120), nullable=True)
+    institution = Column(String(120), nullable=True)
 
     # relationships
     questions = relationship('Question', back_populates='teacher')
