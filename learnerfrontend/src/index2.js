46ffdf5d0877bf2e089b7fda7f8db2cf20e2df14
seyifash <fasugbeoluwaseyi@gmail.com>
2024-03-24 01:28:14 +0100

    fixing merge

--- learnerfrontend/src/index2.js
+++ learnerfrontend/src/index2.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App2 from './App2';
-import { AuthProvider } from './AuthContext'; // Import the AuthProvider
+import { AuthProvider } from './AuthContext';
 
 ReactDOM.render(
     <BrowserRouter>
