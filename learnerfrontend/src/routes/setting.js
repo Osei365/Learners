c557e9b43aede74c899d43ffe8d0ceb990b59a54
seyifash <fasugbeoluwaseyi@gmail.com>
2024-04-30 08:17:14 +0100

    let me breath

---
+++ learnerfrontend/src/routes/setting.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Settings from '../settings/settings';
+
+const Setting = () => {
+  return (
+    <div>
+        <Settings />
+    </div>
+  )
+}
+
+export default Setting
